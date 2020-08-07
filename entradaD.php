1d9ff53d03e0f92cb2e7eb7887d74bbbb2e3f1e3
asc-sistemas <ascarvalho.sousa@gmail.com>
2020-08-06 22:50:40 -0300

    Alterção de comandos no banco

---
+++ entradaD.php
@@ -0,0 +1,18 @@
+<?php
+include("conecta.php");
+
+$nome = $_POST['nome'];
+$valor = $_POST['valor'];
+$dataN = $_POST['data'];
+
+$sql = "INSERT INTO cofre(id, nome, data, valor)VALUES('','$nome', '$dataN', '$valor')";
+
+if (mysqli_query($link, $sql)) {
+      echo "Cadastro com sucesso";
+	 ?> <p><a href="menu.html">Voltar ao Menu</a></p><?php
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($link);
+}
+
+
+?>
